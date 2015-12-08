cdb3f03cb6e67f44bdfff2f8d47822e4ea38043b
zavbak <799000@mail.ru>
2015-12-08 22:01:00 +1000

    Добавил из Тест

--- app/src/main/java/com/anit/transporthttpfor1c/MainActivity.java
+++ app/src/main/java/com/anit/transporthttpfor1c/MainActivity.java
@@ -3,6 +3,9 @@ package com.anit.transporthttpfor1c;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
