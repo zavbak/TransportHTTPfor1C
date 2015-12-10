f445b7d20487473166406f0bf0db35679e43df90
zavbak <799000@mail.ru>
2015-12-10 19:07:50 +1000

    Готовлю Активити

--- app/src/main/java/com/anit/transporthttpfor1c/MainActivity.java
+++ app/src/main/java/com/anit/transporthttpfor1c/MainActivity.java
@@ -2,15 +2,60 @@ package com.anit.transporthttpfor1c;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  *Тест 1
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btExchange = (Button) findViewById(R.id.btExchange);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btExchange.setOnClickListener(this);
     }
+
+
+    void executeExchange(){
+
+
+
+
+
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btExchange:
+
+                executeExchange();
+
+                Toast.makeText(this,"Обмен выполнен",Toast.LENGTH_SHORT).show();
+
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/com/anit/transporthttpfor1c/bin/Exchange1C.java
+++ app/src/main/java/com/anit/transporthttpfor1c/bin/Exchange1C.java
@@ -1,14 +1,45 @@
 package com.anit.transporthttpfor1c.bin;
 
 import java.net.URL;
+import java.util.Map;
 
 /**
  * Created by Александр on 10.12.2015.
  */
 public class Exchange1C {
 
+    /**
+     * Путь к файлу для отправки
+     */
+    String pathOutFile;
 
-    public Boolean executeExchange(String pathOutFile,String pathInFile, URL url){
+    /**
+     * Путь к файлу на получение
+     */
+    String pathInFile;
+
+    /**
+     * Адрес HTTP сервиса
+     */
+    String url;
+
+    /**
+     * Параметры в забросе
+     */
+    Map<String,String> paramReqest;
+
+    /**
+     * Авторизация пользователь в 1С
+     */
+    String user;
+
+    /**
+     * Пароль 1С
+     */
+    String password;
+
+
+    public Boolean executeExchange(){
 
 
         return true;
